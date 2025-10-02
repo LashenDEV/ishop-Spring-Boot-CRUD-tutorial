52f86ad9f16d14273cd18a25d64300df1e2cab4f
LashenDEV <lashendev@gmail.com>
2025-10-02 14:31:48 +0530

    update product done

--- src/main/java/lashen/dev/ishop/controllers/ProductController.java
+++ src/main/java/lashen/dev/ishop/controllers/ProductController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
@@ -82,6 +79,77 @@ public class ProductController {
         product.setImageFileName(storageFileName);
 
         productsRepository.save(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit")
+    public String showEditPage(Model model, @RequestParam int id) {
+
+        try {
+            Product product = productsRepository.findById(id).get();
+            model.addAttribute("product", product);
+
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            productDto.setBrand(product.getBrand());
+            productDto.setCategory(product.getCategory());
+            productDto.setPrice(product.getPrice());
+            productDto.setDescription(product.getDescription());
+
+            model.addAttribute("productDto", productDto);
+
+        } catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+            return "redirect:/products";
+        }
+
+        return "products/editProduct";
+    }
+
+    @PostMapping("edit")
+    public String editProduct(Model model, @RequestParam int id, @Valid @ModelAttribute ProductDto productDto, BindingResult bindingResult) {
+        try {
+            Product product = productsRepository.findById(id).get();
+            model.addAttribute("products", product);
+
+            if(bindingResult.hasErrors()){
+                return "products/editProduct";
+            }
+
+            if(!productDto.getImageFile().isEmpty()){
+                // delete old image
+                String uploadDir = "public/images/";
+                Path oldImagePath = Paths.get(uploadDir + product.getImageFileName());
+
+                try{
+                    Files.delete(oldImagePath);
+                }catch (Exception e){
+                    System.out.println("Exception: " + e.getMessage());
+                }
+
+                // save new image file
+                MultipartFile image = productDto.getImageFile();
+                Date createdAt = new Date();
+                String storageFileName = createdAt.toString() + "_" + image.getOriginalFilename();
+
+                try (InputStream inputStream = image.getInputStream()){
+                    Files.copy(inputStream, Paths.get(uploadDir + storageFileName), StandardCopyOption.REPLACE_EXISTING);
+                }
+
+                product.setImageFileName(storageFileName);
+            }
+
+            product.setName(productDto.getName());
+            product.setBrand(productDto.getBrand());
+            product.setCategory(productDto.getCategory());
+            product.setPrice(productDto.getPrice());
+            product.setDescription(productDto.getDescription());
+
+            productsRepository.save(product);
+        }catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
 
         return "redirect:/products";
     }
